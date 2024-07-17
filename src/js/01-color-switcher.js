f05b14981bc5c6307f27046128fd58e4ed347af9
MystGunn <157536861+MystGunn@users.noreply.github.com>
2024-07-17 21:30:46 +0200

    Tema 9
    Primul exercitiu

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,24 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
+  
+  const startButton = document.querySelector('button[data-start]');
+  const stopButton = document.querySelector('button[data-stop]');
+  let colorInterval = null;
+  
+  startButton.addEventListener('click', () => {
+    if (!colorInterval) {
+      startButton.disabled = true;
+      colorInterval = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+      }, 1000);
+    }
+  });
+  
+  stopButton.addEventListener('click', () => {
+    if (colorInterval) {
+      clearInterval(colorInterval);
+      colorInterval = null;
+      startButton.disabled = false;
+    }
+  });
