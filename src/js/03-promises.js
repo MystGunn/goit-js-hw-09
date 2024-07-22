2013b651f74062d22030aa49c8884aa98732615e
MystGunn <157536861+MystGunn@users.noreply.github.com>
2024-07-22 21:42:30 +0200

    Promises
    Exercitiul 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,34 @@
+document.querySelector('.form').addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const form = event.currentTarget;
+  const firstDelay = Number(form.delay.value);
+  const step = Number(form.step.value);
+  const amount = Number(form.amount.value);
+
+  for (let i = 1; i <= amount; i++) {
+    const delay = firstDelay + (i - 1) * step;
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  }
+});
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Fulfill
+        resolve({ position, delay });
+      } else {
+        // Reject
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
